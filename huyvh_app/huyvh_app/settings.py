7ee371b1a97ff05024cca0fef222e2862cf84005
huyvodl <huyvhunitech@gmail.com>
2015-09-09 16:10:29 +0700

    requirements

--- huyvh_app/huyvh_app/settings.py
+++ huyvh_app/huyvh_app/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'huyvh_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-       'ENGINE': 'django.db.backends.mysql',
- 	   #'ENGINE': 'mysql.connector.django',
-        'NAME': 'python',
-        'USER': 'root',
-        'PASSWORD': 'huyphuong',
-        'HOST': 'localhost',
-        'PORT': '3306',
-     }
- }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+# DATABASES = {
+#     'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#  	   #'ENGINE': 'mysql.connector.django',
+#         'NAME': 'python',
+#         'USER': 'root',
+#         'PASSWORD': 'huyphuong',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#      }
+#  }
 
 
 
