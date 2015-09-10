e44b1c17b8f44859437588359623f4f016849761
huyvodl <huyvhunitech@gmail.com>
2015-09-10 11:36:42 +0700

    fix bug

--- huyvh_app/huyvh_app/wsgi.py
+++ huyvh_app/huyvh_app/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "python.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "huyvh_app.settings")
 #application = get_wsgi_application()
 
 application = Cling(get_wsgi_application())
--- huyvh_app/manage.py
+++ huyvh_app/manage.py
@@ -3,6 +3,6 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "python.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "huyvh_app.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
