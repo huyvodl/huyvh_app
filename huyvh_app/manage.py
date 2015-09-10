b4028b3ed990af9114bff1d9b839840e5693aaab
huyvodl <huyvhunitech@gmail.com>
2015-09-10 11:24:42 +0700

    from configurations.management import execute_from_command_line

--- huyvh_app/manage.py
+++ huyvh_app/manage.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
-from configurations import Configuration
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "python.settings")
-    #from django.core.management import execute_from_command_line
-    from configurations.management import execute_from_command_line
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
